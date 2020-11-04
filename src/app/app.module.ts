6fbdf3745771fbeb5cc6c892d69d2a161cf5c0f4
Aitor <aitordegomar91@hotmail.com>
2020-11-04 20:31:04 +0100

    checkbox, doesnt repeat, individual button to add, fixed if other type is ticked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/currency-list/currency-list.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-currency-list',
+  templateUrl: './currency-list.component.html',
+  styleUrls: ['./currency-list.component.css'],
+})
+export class CurrencyListComponent implements OnInit {
+  rateListA;
+  rateListB;
+  rateListC;
+  isAVisible: boolean = true;
+  isBVisible: boolean = false;
+  isCVisible: boolean = false;
+  i = 0;
+  j =0;
+  l;
+  favouriteList=[];
+  inputs = document.getElementsByTagName('input');
+
+  constructor(private ApiService: ApiService) {}
+  ngOnInit() {
+    this.ApiService.getRateA().subscribe((data) => {
+      this.rateListA = data[0].rates;
+    });
+    this.ApiService.getRateB().subscribe((data) => {
+      this.rateListB = data[0].rates;
+    });
+    this.ApiService.getRateC().subscribe((data) => {
+      this.rateListC = data[0].rates;
+    });
+  }
+
+  showA = function () {
+    this.isAVisible = true;
+    this.isBVisible = false;
+    this.isCVisible = false;
+  };
+  showB() {
+    this.isAVisible = false;
+    this.isBVisible = true;
+    this.isCVisible = false;
+  }
+  showC() {
+    this.isAVisible = false;
+    this.isBVisible = false;
+    this.isCVisible = true;
+  }
+
+  addFavA() {
+    for (this.i = 0; this.i < this.rateListA.length; this.i++) {
+      if(this.inputs[this.i + 1].checked && this.favouriteList.includes(this.rateListA[this.i].code)){
+        continue;
+      }else{
+      if (this.inputs[this.i].checked) {
+        this.favouriteList.push(this.rateListA[this.i].code);
+      }
+    }
+    }
+    console.log(this.favouriteList);
+    this.i =0;
+  }
+  addFavB() {
+    for (this.j = 0; this.j < this.rateListB.length; this.j++) {
+      this.l = this.j + this.rateListA.length;
+      if(this.inputs[this.l].checked && this.favouriteList.includes(this.rateListB[this.j].code)){
+        continue;
+      }else{
+        if(this.inputs[parseInt(this.rateListA.length)+ this.j].checked){
+        this.favouriteList.push(this.rateListB[this.j].code);
+      }
+    }
+  }
+  console.log(this.favouriteList);
+    this.j=0;
+  }
+}
